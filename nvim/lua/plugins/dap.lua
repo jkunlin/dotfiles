edda98a5cfa86d46d76a9e45fb6c2b277255fba0
jkunlin <jkunlin@gmail.com>
2024-04-13 01:43:54 +0000

    feat: dap support coredump; dap python

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -47,6 +47,32 @@ return {
             type = "cppdbg",
             request = "launch",
             program = os.getenv("HOME") .. "/smt/stp/build/stp",
+            -- coreDumpPath = function()
+            --   return vim.fn.input("Path to coredump: ", "/var/lib/apport/coredump/", "file")
+            -- end,
+            coreDumpPath = function()
+              local directory = "/var/lib/apport/coredump/"
+              local scan_command = "ls -t " .. directory .. " | head -n1"
+              local handle = io.popen(scan_command)
+              local result = handle:read("*a")
+              handle:close()
+              local newest_file = result:gsub("\n$", "") -- 去除结果字符串末尾的换行符
+              return directory .. "/" .. newest_file
+            end,
+            cwd = "${workspaceFolder}",
+            stopOnEntry = true,
+            -- setupCommands = {
+            --   {
+            --     text = "-enable-pretty-printing",
+            --     description = "enable pretty printing",
+            --     ignoreFailures = false,
+            --   },
+          },
+          {
+            name = "stp-tmp",
+            type = "cppdbg",
+            request = "launch",
+            program = os.getenv("HOME") .. "/smt/stp/build/stp",
             cwd = "${workspaceFolder}",
             stopOnEntry = true,
             setupCommands = {
@@ -56,7 +82,7 @@ return {
                 ignoreFailures = false,
               },
               {
-                text = "set args /home/linjk/smt/2021_QF_BV/Sage2/bench_13275.smt2",
+                text = "set args /home/linjk/smt/stp/build/test.smt2",
                 description = "instance",
                 ignoreFailures = false,
               },
@@ -90,6 +116,37 @@ return {
           },
         }
       end
+
+      dap.adapters.python = {
+        type = "executable",
+        command = "python",
+        args = { "-m", "debugpy.adapter" },
+      }
+
+      dap.configurations.python = {
+        {
+          name = "Launch file",
+          type = "python",
+          request = "launch",
+          program = "${file}", -- current file
+          -- If true, the console opens at the start of the session; otherwise, it's closed.
+          console = "integratedTerminal",
+          args = {
+            "--file",
+            "/pub/netdisk1/linjk/smt/bv/benchmarks/QF_BV-42472/spear/samba_v3.0.24/bin_libsmbsharemodes_vc5774.smt2",
+            "--output-dir",
+            "/home/linjk/smt/BVPartition/results/raw-QF_BV-42472/debug/temp",
+            "--partitioner",
+            "/home/linjk/smt/BVPartition/src/partitioner/build/stp",
+            "--solver",
+            "/home/linjk/smt/stp-cadical/build/stp",
+            "--max-running-tasks",
+            "1",
+            "--time-limit",
+            "200",
+          },
+        },
+      }
     end,
   },
 }
